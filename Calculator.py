c02c8671ddd7e2aa81c896e98df0b70712531551
Allen <dash.8123@gmail.com>
2018-01-11 17:27:08 -0700

    Changed menu
    Instead of using the 1-6, I made it so the user can type out 'add', 'subtract', 'sub', 'multiply', 'multi', ect to select an opperation.

--- Calculator.py
+++ Calculator.py
@@ -6,63 +6,72 @@
 ** for power
 % for modulo
 '''
-def add(num1,num2):
+
+# calc functions
+def add(num1, num2):
     return num1 + num2
 
-def subtract(num1,num2):
+
+def subtract(num1, num2):
     return num1 - num2
 
-def multiply(num1,num2):
+
+def multiply(num1, num2):
     return num1 * num2
 
-def divide(num1,num2):
-    return num1 // num2
 
-def power(num1,num2):
+def divide(num1, num2):
+    return num1 / num2
+
+
+def power(num1, num2):
     return num1 ** num2
 
-#main loop
+#main loops
 while True:
+
+    #menu
     print('\nSelect operation.')
-    print('1.Add')
-    print('2.Subtract')
-    print('3.Multiply')
-    print('4.Divide')
-    print('5.Powers')
-    print('6.Exit')
+    print('Add?')
+    print('Subtract?')
+    print('Multiply?')
+    print('Divide?')
+    print('Powers?')
+    print('Exit?')
 
-    choice = input('\nEnter choice(1/2/3/4/5/6): ')
+    #user input
+    choice = input('\nWhat would you like to do: ')
+    choice = choice.split()[0][0]
 
-    if choice == '6':
+    # Choice opperations
+    if choice == 'a':  # Addition
+        num1 = float(input('\nEnter first number: '))
+        num2 = float(input('Enter second number: '))
+        print(num1, '+', num2, '=', add(num1, num2))
+
+    elif choice == 's':  # Subtraction
+        num1 = float(input('\nEnter first number: '))
+        num2 = float(input('Enter second number: '))
+        print(num1, '-', num2, '=', subtract(num1, num2))
+
+    elif choice == 'm':  # Multiplication
+        num1 = float(input('\nEnter first number: '))
+        num2 = float(input('Enter second number: '))
+        print(num1, 'x', num2, '=', multiply(num1, num2))
+
+    elif choice == 'd':  # Divistion
+        num1 = float(input('\nEnter first number: '))
+        num2 = float(input('Enter second number: '))
+        print(num1, "/", num2, "=", divide(num1, num2))
+
+    elif choice == 'p':  # Powers
+        num1 = float(input('\nEnter first number: '))
+        num2 = float(input('Enter the power of the number: '))
+        print(num1, '^', num2, '=', power(num1, num2))
+
+    elif choice == 'e':  # Exit
         break
-               
-    if choice == '1':
-        num1 = int(input('\nEnter first number: '))
-        num2 = int(input('Enter second number: '))
-        print (num1, '+', num2, '=', add(num1,num2))
 
-    elif choice == '2':
-        num1 = int(input('\nEnter first number: '))
-        num2 = int(input('Enter second number: '))
-        print (num1, '-', num2, '=', subtract(num1,num2))
-
-    elif choice == '3':
-        num1 = int(input('\nEnter first number: '))
-        num2 = int(input('Enter second number: '))
-        print (num1, 'x', num2, '=', multiply(num1,num2))
-
-    elif choice == '4':
-        num1 = int(input('\nEnter first number: '))
-        num2 = int(input('Enter second number: '))
-        print(num1, "/", num2, "=", divide(num1,num2))
-
-    elif choice == '5':
-        num1 = int(input('\nEnter first number: '))
-        num2 = int(input('What is the power of this number: '))
-        print (num1, '^', num2, '=', power(num1,num2))
-
-    elif choice == '6':
-        break
-
-    else:
-        print('\nInvalid Selection!\n Please choise a number between 1-6')
+    else:  # if user doesn't enter a valid operator
+        print('\nInvalid Selection! \n'
+              'Please enter a valid operator!')
